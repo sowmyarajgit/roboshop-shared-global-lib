5b7b50fbb65d782bcb319ecbaafc7aa6b402a0f7
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-26 23:24:13 +0530

    git hub adding

---
+++ vars/ci.groovy
@@ -0,0 +1,36 @@
+def call() {
+
+    
+pipeline {
+    agent {
+     label 'ansible'   
+    }
+ stages  {
+    stage ('compile/build') {
+        steps {
+            echo 'compile/build'
+        }
+
+    }
+    stage ('unittest') {
+        steps {
+            echo 'unittest'
+        }
+
+    }
+    stage ('qualitycontrol') {
+        steps {
+            echo 'qualitycontrol'
+        }
+
+    }
+    stage ('upload file to centalized place') {
+        steps {
+            echo 'upload file to centralized place'
+        }
+
+    }
+ } 
+
+}
+}
