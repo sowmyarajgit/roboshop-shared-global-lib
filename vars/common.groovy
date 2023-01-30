46089707a0d5691c79197de38a643f10f1da0b42
sowmyaraj <sowmyaraj435@gmail.com>
2023-01-30 16:07:34 +0530

    exception handling mail

--- vars/common.groovy
+++ vars/common.groovy
@@ -28,5 +28,5 @@ if (app_lang == "python") {
 
 def email(email_notes){
 
-  mail bcc: '', body: "JOB FAILED - ${JOB_BASE_NAME} \n JOB URL - ${JOB_URL}", cc: '', from: 'sowmyaraj435@gmail.com', replyTo: '', subject: 'Test from jenkins', to: 'sowmyaraj435@gmail.com'
+  mail bcc: '', body: "JOB FAILED - ${JOB_BASE_NAME} \n JOB URL - ${JOB_URL}", cc: '', from: 'sowmyaraj435@gmail.com', replyTo: '', subject: 'Test from jenkins - ${JOB_BASE_NAME}', to: 'sowmyaraj435@gmail.com'
 }
