87f277ae669672100e5f08224514b96ea646307f
sowmyaraj <sowmyaraj435@gmail.com>
2023-02-04 20:18:52 +0530

    sonar-password

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -28,11 +28,12 @@ pipeline {
         SONAR_PASS ='$(aws ssm get-parameters --region us-east-1 --names sonarqube_pass --query Parameters[0].Value --with-decryption | sed \'s/"//g\')'
     }
         steps {
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'PASSWORD']]]){
             sh  "sonar-scanner -Dsonar.host.url=http://172.31.4.194:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectkey=cart"
     }
         }
 
-    
+ }
     stage ('upload file to centalized place') {
         steps {
             echo 'upload file to centralized place'
